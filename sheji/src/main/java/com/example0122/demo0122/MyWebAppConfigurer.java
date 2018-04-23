77b252cf047d012ea907a7a7d139b00e00dd6fe4
强枭峰 <747070686@qq.com>
2018-04-23 19:44:14 +0800

    修改ip地址为服务器地址

--- sheji/src/main/java/com/example0122/demo0122/MyWebAppConfigurer.java
+++ sheji/src/main/java/com/example0122/demo0122/MyWebAppConfigurer.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/Users/qxf/IdeaProjects/Graduation-project/sheji/src/main/resources/**").addResourceLocations("classpath:");
+        registry.addResourceHandler("/root/sheji/**").addResourceLocations("classpath:");
         super.addResourceHandlers(registry);
     }
 }
